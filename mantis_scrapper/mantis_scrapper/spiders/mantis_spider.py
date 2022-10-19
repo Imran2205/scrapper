b7272fdf2f3cdeb6b9f2fcd954c7e0159268be87
imran2205 <imrankabir1996@gmail.com>
2022-10-19 17:38:43 +0600

    commit 1

--- mantis_scrapper/mantis_scrapper/spiders/mantis_spider.py
+++ mantis_scrapper/mantis_scrapper/spiders/mantis_spider.py
@@ -6,15 +6,59 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://quotes.toscrape.com/page/1/',
-            'https://quotes.toscrape.com/page/2/',
+            'file://C:/Users/admin/Desktop/desktop/web/scrapper/html/Mantis.html',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    def get_text_from_span(self, span_ele):
+        ret_str = ""
+        if span_ele.get().startswith('<span'):
+            if span_ele.xpath("./*"):
+                ret_str = self.get_text_from_span(span_ele.xpath("./*"))
+            else:
+                ret_str = span_ele.css('span::text').get()
+        elif span_ele.get().startswith('<a'):
+            a_tags = span_ele.css('a')
+            for cnt, a in enumerate(a_tags):
+                if cnt == 1 and len(a_tags) == 2:
+                    ret_str += f" [{a.css('a::text').get()}]"
+                else:
+                    ret_str += a.css('a::text').get()
+        elif span_ele.get().startswith('<input'):
+            ret_str = span_ele.css('input::attr(value)').get()
+        else:
+            ret_str = span_ele.css('span::text').get()
+
+        return ret_str
+
     def parse(self, response):
-        page = response.url.split("/")[-2]
+        print(response)
+        page = response.url.split("/")[-1].split('.')[0]
+        print('#########################################################################', page)
         filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        device_summary_divs = response.css('div.device-summary-container')
+        device_summary_tables = device_summary_divs.css('div.device-summary-table')
+        for table in device_summary_tables:
+            tab_head = table.css('h6::text')[0].get()
+            print('************************************', tab_head)
+
+            keys = table.css('span.label')
+            values = table.css('span.label-value')
+
+            sub_table_dict = {}
+            for i, key in enumerate(keys):
+                key_span = key
+                val_span = values[i]
+
+                # print(key_span, '||', key_span.xpath("./*").get())
+                # print(val_span.get(), '||', key_span.xpath("./*").get())
+
+                key_val = self.get_text_from_span(key_span)
+                val_val = self.get_text_from_span(val_span)
+
+                print(key_val, ':', val_val)
+
+            print('#########################################################################', len(keys), len(values))
+
         self.log(f'Saved file {filename}')
