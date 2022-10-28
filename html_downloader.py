d8571c0f8c4c6d4e9a3f8a60f56724b4da11abe0
imran2205 <imrankabir1996@gmail.com>
2022-10-28 12:56:37 +0600

    commit 1

--- html_downloader.py
+++ html_downloader.py
@@ -6,9 +6,13 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time
 from scrapy.crawler import CrawlerProcess
 from mantis_scrapper.mantis_scrapper.spiders.mantis_spider import MantisSpider
+# from pyvirtualdisplay import Display
 
 task_list = [
     '2166972',
@@ -22,28 +26,60 @@ urls = [f"{base_url}{task}" for task in task_list]
 output_json_path = r'C:\Users\admin\Desktop\desktop\web\scrapper\json'
 
 proc = CrawlerProcess()
+#
+# CHROME_PATH = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+# CHROMEDRIVER_PATH = r'C:\chromedriver.exe'
+WINDOW_SIZE = "1000,600"
 
-for url in urls:
-    driver = webdriver.Chrome(r'C:\chromedriver.exe')
+chrome_options = Options()
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+# chrome_options.add_argument("--headless")
+# chrome_options.binary_location = CHROME_PATH
+# chrome_options.add_argument("headless")
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_experimental_option("prefs", {"download.default_directory": "/databricks/driver"})
+driver = webdriver.Chrome(
+    executable_path=r'C:\chromedriver.exe',
+    chrome_options=chrome_options
+)
+
+for count, url in enumerate(urls):
+    # options = webdriver.ChromeOptions()
+    # options.binary_location = r'C:\chromedriver.exe'
+    # options.add_argument("--log-level=3")
+
     driver.get(url)
     job_id = url.split("/")[-1]
     html_out_file = f'C:/Users/admin/Desktop/desktop/web/scrapper/html/{job_id}.html'
 
-    driver.find_element(By.ID, 'details-button').click()
-    driver.find_element(By.ID, 'proceed-link').click()
+    # print("##################################################################################")
+    # print(driver.page_source)
 
-    timeout = 50
-    try:
-        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'full-width-input'))
-        WebDriverWait(driver, timeout).until(element_present)
-    except TimeoutException:
-        print("Timed out waiting for page to load")
+    if count == 0:
+        timeout = 50
+        try:
+            element_present = EC.presence_of_element_located((By.ID, 'details-button'))
+            WebDriverWait(driver, timeout).until(element_present)
+        except TimeoutException:
+            print("Timed out waiting for page to load")
 
-    driver.find_element(By.NAME, 'username').send_keys('asad')
-    driver.find_element(By.NAME, 'password').send_keys('GFULKA@@@1234')
-    driver.find_element(By.CLASS_NAME, 'mat-raised-button').click()
+        driver.find_element(By.ID, 'details-button').click()
+        driver.find_element(By.ID, 'proceed-link').click()
 
-    time.sleep(10)
+        timeout = 50
+        try:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, 'full-width-input'))
+            WebDriverWait(driver, timeout).until(element_present)
+        except TimeoutException:
+            print("Timed out waiting for page to load")
+
+        driver.find_element(By.NAME, 'username').send_keys('asad')
+        driver.find_element(By.NAME, 'password').send_keys('GFULKA@@@1234')
+        driver.find_element(By.CLASS_NAME, 'mat-raised-button').click()
+
+        time.sleep(10)
 
     try:
         element_present = EC.presence_of_element_located((By.CLASS_NAME, 'device-summary-container'))
@@ -53,14 +89,13 @@ for url in urls:
 
     html = driver.page_source
 
-    driver.close()
-    driver.quit()
-
     with open(html_out_file, 'w') as f:
         f.write(html)
 
     proc.crawl(MantisSpider, stat_urls=[f'file://{html_out_file}'], out_path=output_json_path)
 
+driver.close()
+driver.quit()
 proc.start()
 
 json_files = os.listdir(output_json_path)
