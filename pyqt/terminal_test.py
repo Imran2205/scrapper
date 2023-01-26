4a1698e6fac1da8f6e23da521ff63e0247368ef4
imran2205 <imrankabir1996@gmail.com>
2023-01-26 13:49:09 +0600

    commit 1

--- pyqt/terminal_test.py
+++ pyqt/terminal_test.py
@@ -1,8 +1,8 @@
-import pexpect
-
-# start a child  process with spawn
-# It just echos  geeksforgeeks
-child = pexpect.spawn("echo geeksforgeeks")
-
-# prints he matched index of string.
-print(child.expect(["hai", "welcome", "geeksforgeeks"]))
+# import pexpect
+#
+# # start a child  process with spawn
+# # It just echos  geeksforgeeks
+# child = pexpect.spawn("echo geeksforgeeks")
+#
+# # prints he matched index of string.
+# print(child.expect(["hai", "welcome", "geeksforgeeks"]))
